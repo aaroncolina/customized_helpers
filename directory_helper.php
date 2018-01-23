6e5f2a36952e0bdeb7821bef94211bc6388a40cc
Aaron.Colina <Aaron.Colina@maximintegrated.com>
2018-01-23 11:30:06 +0800

    Added delete_folder functionality on Directory helper
    Added delete_folder functionality on Directory helper

--- directory_helper.php
+++ directory_helper.php
@@ -21,4 +21,26 @@ function get_rfq_upload_path($rid, $path_type=0){
 }
 
 
+
+function delete_folder($str) {
+    //It it's a file.
+    if (is_file($str)) {
+        //Attempt to delete it.
+        return unlink($str);
+    }
+    //If it's a directory.
+    elseif (is_dir($str)) {
+        //Get a list of the files in this directory.
+        $scan = glob(rtrim($str,'/').'/*');
+        //Loop through the list of files.
+        foreach($scan as $index=>$path) {
+            //Call our recursive function.
+            delete_folder($path);
+        }
+        //Remove the directory itself.
+        return @rmdir($str);
+    }
+}
+
+
 ?>
